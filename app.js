bd7e0a74bd75dbd008d9debba201f7b312fe8cb8
David Lindner <davidlindnerjr@gmail.com>
2020-08-24 14:26:02 -0700

    Updated Porfolio commit-1

---
+++ app.js
@@ -0,0 +1,32 @@
+//Responsive Navbar
+const navSlide = () =>{
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    //toggle nav
+    burger.addEventListener('click', ()=>{
+        nav.classList.toggle('nav-active');
+
+        //animate links
+        navLinks.forEach((link, index)=>{
+            if(link.style.animation){
+                link.style.animation = '';
+            }
+            else{
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index/7 + 0.3}s`; //equal delay for each link
+            }
+        });
+
+        //burger animation
+        burger.classList.toggle('toggle');
+    });
+}
+
+//Conatiner for all functionality
+const app = () =>{
+    navSlide();
+}
+
+//Envoke app
+app();
